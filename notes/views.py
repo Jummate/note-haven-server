0ad10d69cb0509f10fb6567ce3402b52787d20fc
Jummate <yakubjumat@gmail.com>
2025-07-15 19:36:32 +0100

    feat(note):create endpoint for getting notes and tags

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from notes.models import Note
 from notes.serialisers import NoteSerializer
 
 
@@ -16,8 +17,10 @@ def notes_view(request):
             return Response({"message":"Note created succesfully", "data":response_data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
-        print("okay")
-        return Response({"message":"notes"}, status=status.HTTP_200_OK)
+        user = request.user
+        notes = Note.objects.filter(user=user).order_by('-updated_at')
+        response_data = NoteSerializer(notes, many=True).data
+        return Response(response_data, status=status.HTTP_200_OK)
 
 @api_view(['GET','PUT','PATCH','DELETE'])
 def note_detail(request):
--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from notes.serialisers import TagSerializer
+
+@api_view(['GET'])
+def tags_view(request):
+    try:
+        tags = request.user.tags.all()
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response(
+            {'error': 'Something went wrong while fetching tags.'},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
