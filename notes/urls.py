4366cf91820207d358d33ba92e943fd7f2b315fc
Jummate <yakubjumat@gmail.com>
2025-07-19 23:05:12 +0100

    feat(archive): create endpoint for note archive

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.notes_view, name="notes"),
     path('<uuid:note_id>/', views.note_detail, name="note-detail"),
+    path('<uuid:note_id>/archive', views.archive_note, name="archive-note"),
 ]
