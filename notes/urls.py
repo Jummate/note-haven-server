59cc92d7d4a43e1d2fb0ca4205e731377a112ad3
Jummate <yakubjumat@gmail.com>
2025-07-19 23:17:47 +0100

    feat(restore): create endpoint for note restore

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.notes_view, name="notes"),
     path('<uuid:note_id>/', views.note_detail, name="note-detail"),
     path('<uuid:note_id>/archive', views.archive_note, name="archive-note"),
+    path('<uuid:note_id>/restore', views.restore_note, name="restore-note"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -41,4 +41,14 @@ def archive_note(request, note_id):
     note.is_archived = True
     note.save()
     return Response({"message":"Note archived succesfully"}, status=status.HTTP_200_OK)
+
+@api_view(['PATCH'])
+def restore_note(request, note_id):
+    try:
+        note = Note.objects.get(id=note_id, user=request.user)
+    except note.DoesNotExist:
+        return Response({"error": "Note not found"}, status=status.HTTP_404_NOT_FOUND)
+    note.is_archived = False
+    note.save()
+    return Response({"message":"Note restored succesfully"}, status=status.HTTP_200_OK)
         
